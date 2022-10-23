7d55fa32af9664e3f3cb0eeef0fbf1c36919719a
Parker Thomson <pdthomson1239@gmail.com>
2022-10-23 17:18:43 -0400

    mod 1 JS project iteration 1-4

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,53 @@
-function nameMenuItem() {
-
-}
-
-
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
+function nameMenuItem(...food) {
+  var deliciousFood = 'Delicious' + ' ' + food;
+  return deliciousFood;
+}
 
+function createMenuItem(name, price, type){
+var menuObject = {
+  name: name,
+  price: price,
+  type: type
+};
+
+return menuObject;
+}
+
+function addIngredients(food, ingredients){
+  if (ingredients.includes(food)){
+    return ingredients;
+  } else
+  {ingredients.push(food)};
+}
+
+function formatPrice(unformattedPrice) {
+  return `$${unformattedPrice}`
+}
+
+function decreasePrice(priceInteger) {
+  // var tenPercentDiscount = priceInteger * .1;
+  // var discountedPrice = priceInteger - tenPercentDiscount;
+
+  // return discountedPrice
+  return priceInteger * .9
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipeObject = {
+    title: title,
+    ingredients: ingredients, 
+    type: type
+  };
+
+  return recipeObject;
+}
+  
--- src/order.js
+++ src/order.js
@@ -1,7 +1,39 @@
+function takeOrder(order, orderArray){
+ if(orderArray.length === 3){
+  return orderArray;
+ } else {
+  return orderArray.push(order)}
+}
+
+function refundOrder(orderNum, orderArray) {
+  for (var i = 0; i < orderArray.length; i++) {
+    if (orderArray[i].orderNumber === orderNum) {
+      orderArray.splice(i, 1);
+    }
+  }
+}
+
+function listItems(orderArray) {
+  var itemArray = []
+  for (var i = 0; i < orderArray.length; i++) {
+    itemArray.push(orderArray[i].item)};
+    return itemArray.join(', ');
+  }
+
+  function searchOrder(orderArray, foodName) {
+    for (var i = 0; i < orderArray.length; i++) {
+      if(orderArray[i].item === foodName)
+      {
+      return true;
+    }
+    };
+      return false;
+      
+  }
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
